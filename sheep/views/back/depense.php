52f997c64a36321c9ebe0bb35b57a7357a0973b7
Juratul <julienverna9@gmail.com>
2017-11-23 16:58:05 +0100

    Add files via upload

---
+++ sheep/views/back/depense.php
@@ -0,0 +1,24 @@
+<?php ob_start() ; ?>
+<?php include __DIR__ . '/../partials/nav.php'; ?>
+
+<?php echo "<h2>Ajouter des nouvelles dépenses</h2>"; ?>
+
+<?php $data   = filter_input(INPUT_POST, 'data'); ?>
+
+<form action="#" method="post">
+
+	Titre<input type="text" name="titre" placeholder="Titre de l'achat"><br>
+	Montant<input type="text" name="montant" placeholder="Prix de l'achat"><br>
+	Date<input type="date" name="date"><br>
+
+	<?php foreach ($userName as $name) :  ?>
+    	<input type="checkbox" name=""> <?php echo htmlentities($name['name']) ."<br>" ;?>
+	<?php endforeach; ?>
+
+	Vérifier le montant<input type="checkbox" name="verifierMontant"><br>
+	<input type="submit" value="Ajouter" name="ajouter">
+</form>
+
+<?php $content = ob_get_clean() ; ?>
+
+<?php include __DIR__ . '/../layouts/master.php' ?>
